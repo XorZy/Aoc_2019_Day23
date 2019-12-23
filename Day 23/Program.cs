81f68e3edc3f3239cafe5b9d795243ef6d4c25f1
XozrZy <sb06390@hotmail.fr>
2019-12-23 19:51:37 +0100

    Initial Commit

---
+++ Day 23/Program.cs
@@ -0,0 +1,108 @@
+﻿using LibCompute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Day_23
+{
+    internal class Program
+    {
+        private static Dictionary<long, Queue<long[]>> _buffers;
+
+        private static void Main(string[] args)
+        {
+            const int computerCount = 50;
+
+            _buffers = new Dictionary<long, Queue<long[]>>();
+            var computers = new IntcodeComputer[computerCount];
+            var ios = new IOPipe[computerCount];
+
+            bool firstSolution = false;
+
+            long lastNatX = -1, lastNatY = -1;
+            long natX = 0, natY = 0;
+
+            long ticks = 0;
+            long lastPacketTick = 0;
+            const int idleThreshold = 1250; // a little bit high just to be sure
+
+            for (int i = 0; i < computerCount; i++)
+            {
+                var io = new IOPipe();
+                int cp = i;
+                ios[i] = io;
+                io.InputInt(i);
+                var buffer = new Queue<long[]>();
+
+                _buffers.Add(i, buffer);
+
+                computers[i] = new IntcodeComputer($"Computer {i}", "input", io);
+
+                io.FireEveryNbOutput = 3;
+
+                io.ReadingInt += (s, e) =>
+                  {
+                      if (buffer.Count == 0)
+                      {
+                          io.InputInt(-1);
+                      }
+                      else
+                      {
+                          var packet = buffer.Dequeue();
+
+                          io.InputInt(packet[0]);
+                          io.InputInt(packet[1]);
+                      }
+                  };
+
+                io.IntOuputted += (s, e) =>
+                  {
+                      var address = io.ReadOutputInt();
+                      var X = io.ReadOutputInt();
+                      var Y = io.ReadOutputInt();
+                      lastPacketTick = ticks;
+
+                      if (address == 255)
+                      {
+                          natX = X;
+                          natY = Y;
+
+                          if (!firstSolution)
+                          {
+                              Console.WriteLine(natY);
+                              firstSolution = true;
+                          }
+                      }
+                      else
+                      {
+                          _buffers[address].Enqueue(new long[] { X, Y });
+                      }
+                  };
+            }
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                foreach (var computer in computers)
+                    computer.Step();
+
+                if (_buffers.All(x => x.Value.Count == 0) && ticks - lastPacketTick > idleThreshold)
+                {
+                    if (lastNatY == natY)
+                    {
+                        Console.WriteLine(natY);
+                        keepGoing = false;
+                    }
+
+                    _buffers[0].Enqueue(new[] { natX, natY });
+                    lastPacketTick = ticks;
+
+                    lastNatX = natX;
+                    lastNatY = natY;
+                }
+
+                ticks++;
+            }
+        }
+    }
+}
---
+++ LibCompute/ComputerBase.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibCompute
+{
+    public enum MEMORY_ACCESS_MODE
+    {
+        POSITION = 0,
+        IMMEDIATE = 1,
+        RELATIVE = 2
+    }
+
+    public abstract class ComputerBase
+    {
+        private List<long> _memory;
+        private long PC { get; set; }
+        public string Name { get; }
+
+        public bool ProgramTerminated { get; protected set; }
+
+        public bool IsRunning { get; protected set; }
+
+        public bool Aborted { get; private set; }
+
+        #region MEMORY_ACCESS
+
+        protected long MemoryReadAt(long index)
+        {
+            Allocate(index);
+            return _memory[(int)index];
+        }
+
+        public void Abort()
+        {
+            Aborted = true;
+        }
+
+        public void MemoryWriteAt(long index, long value)
+        {
+            Allocate(index);
+            _memory[(int)index] = value;
+        }
+
+        private void Allocate(long index)
+        {
+            if (index >= _memory.Count)
+            {
+                var length = index - _memory.Count + 1;
+                for (int i = 0; i < length; i++)
+                    _memory.Add(0);
+            }
+        }
+
+        protected void Dump()
+        {
+            Console.WriteLine($"PC:{PC}");
+
+            var lines = Math.Ceiling(_memory.Count / 10.0);
+
+            for (int i = 0; i < lines; i++)
+            {
+                Console.WriteLine($"{i * 10:0000}: {string.Join(" ", _memory.Skip(i * 10).Take(10))}");
+            }
+        }
+
+        protected long MemoryReadOne()
+        {
+            return MemoryReadAt((int)PC++);
+        }
+
+        public abstract bool Step();
+
+        public void Run()
+        {
+            // Console.WriteLine("Computer started");
+            IsRunning = true;
+            while (!Aborted && Step()) ;
+            ProgramTerminated = true;
+            IsRunning = false;
+        }
+
+        protected ComputerBase(string name, long[] memory, IInputOutput io)
+        {
+            Name = name;
+            _memory = new List<long>(memory);
+            IO = io;
+        }
+
+        #endregion MEMORY_ACCESS
+
+        protected void JumpTo(long index)
+        {
+            PC = index;
+        }
+
+        public IInputOutput IO { get; }
+    }
+}
